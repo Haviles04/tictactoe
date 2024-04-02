f5163615ace5cee86345b6ea7bb8a43f913e0431
Henry Aviles <haviles04@gmail.com>
2024-04-02 09:42:58 -0400

    chore: removed log

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,5 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err);
   const statusCode = res.statusCode ?? 500;
   res.status(statusCode).json({ error: { message: err.message } });
   next();
